bb3fad29cf09404cdc3b7b03537936b4cf8c3fe5
theamk <code@theamk.com>
2021-05-31 16:41:59 -0400

    Add support for power-down button
    Also update LCD library to accomodate new display

--- syslcd.py
+++ syslcd.py
@@ -2,12 +2,22 @@
 import time
 import os
 import sys
+import subprocess
 
 sys.path.append(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lcd_5110'))
 from lcd5110 import LCD5110
 
+import RPi.GPIO as GPIO
 import wpa_info
+
+# A poweoff button support: the button should be connected between this pin 
+# and GND. It's a board pin number because LCD library sets board mode
+POWERDN_PIN = 22
+# To prevent accidental activation, the button must be held for correct number of
+# cycles, here is that range:
+POWERDOWN_ARM_CNT   = 10
+POWEDOWN_CANCEL_CNT = 15
 
 def short_timestamp():
     return time.strftime('%b%d %H:%M:%S', time.localtime())
@@ -20,10 +30,11 @@ def main():
 
     wifi_info = wpa_info.WifiWPAInfo()
     sys.stdout.flush()
-    #os.system("gpio readall")
     update_count = 0
+    btn_hit_count = 0
 
-
+    if POWERDN_PIN is not None:
+        GPIO.setup([POWERDN_PIN], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def update_data(wifi):
         with open('/proc/uptime', 'r') as f:
@@ -33,8 +44,8 @@ def main():
             os.uname().nodename,  # hostname
             (wifi.status or wifi.ip)[-14:],
             "%4s %s" % (wifi.rssi or "??", wifi.ssid or '<no-ssid>'),
-            "",  # TODO unused?
-            time.strftime("Up %b%d %H:%M", time.localtime(time.time() - uptime_s)),
+            "",  # TODO put CPU/disk usag here
+            time.strftime("%b%d %H:%M UP", time.localtime(time.time() - uptime_s)),
             short_timestamp()
         ]
 
@@ -46,6 +57,30 @@ def main():
             wifi = wifi_info.poll()
 
             new_lines = update_data(wifi)
+
+            if POWERDN_PIN is not None and not GPIO.input(POWERDN_PIN):
+                # button hit! Increase the counter and let user know.
+                if not btn_hit_count:
+                    print('button pressed')
+                btn_hit_count += 1
+                if btn_hit_count < POWERDOWN_ARM_CNT:
+                    new_lines[0] = 'Poweroff? %d' % (POWERDOWN_ARM_CNT - btn_hit_count)
+                elif btn_hit_count < POWEDOWN_CANCEL_CNT:
+                    new_lines[0] = 'RELEASE to off'
+                elif btn_hit_count < 3 * POWEDOWN_CANCEL_CNT:
+                    new_lines[0] = 'pwroff CANCEL'
+                # else button is stuck, so stop showing anything...
+            elif POWERDOWN_ARM_CNT <= btn_hit_count < POWEDOWN_CANCEL_CNT:
+                # button released while armed -- go do shutdown
+                print('Button released at', btn_hit_count, ', starting shutdown')
+                btn_hit_count = 0
+                sys.stdout.flush()
+                new_lines[0] = 'SHUTDOWN!'
+                subprocess.Popen("sudo poweroff", shell=True)
+            elif btn_hit_count:
+                print('Button released at', btn_hit_count)
+                btn_hit_count = 0
+
             update_count += 1
             if (update_count % 30) == 0:
                 last_lines = [''] * 6
